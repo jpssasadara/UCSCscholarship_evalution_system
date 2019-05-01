530d0487552dff3eb5f8ea496f3f64800495c2fb
jps sasadara <jpssasadara1995@gmail.cpm>
2019-05-02 01:17:05 +0530

    change

--- UCSC/src/app/admin-for-welfare-schol/admin-for-welfare-schol.component.ts
+++ UCSC/src/app/admin-for-welfare-schol/admin-for-welfare-schol.component.ts
@@ -10,12 +10,77 @@ export class AdminForWelfareScholComponent implements OnInit {
   public students = [];
   public listOfStudents: Student[];
   FatherMonth = "dffgf";
+
+  //initialization of default marks 
+  distance1= 0 ;
+  distance2 = 0;
+  distance3= 0 ;
+  distance4 = 0;
+  distance5 = 0;
+  distance6 = 0;
+  Income1 = 0;
+  Income2 = 0;
+  Income3 = 0; 
+  Income4 = 0;
+  Income5 = 0;
+  Income6 = 0;
+  special1 = 0;
+  special2 = 0;
+  special3 = 0;
+  special4 = 0;
+  special5 = 0;
+  special6 = 0;
+  special7 = 0;
   constructor(private _studentwelfareview: StudentWellfareService) { }
 
   ngOnInit() {
     this._studentwelfareview.getStudents()
       .subscribe(data => this.students = data);
 
+  }
+  //initialization of default marks 
+  setDefault(){
+    this.distance1 = 55;
+    this.distance2 = 50;
+    this.distance3 = 45;
+    this.distance4 = 40;
+    this.distance5 = 35;
+    this.distance6 = 30;
+    this.Income1 = 25;
+    this.Income2 = 20;
+    this.Income3 = 15; 
+    this.Income4 = 10;
+    this.Income5 = 7;
+    this.Income6 = 5;
+    this.special1 = 10;
+    this.special2 = 7;
+    this.special3 = 5;
+    this.special4 = 5;
+    this.special5 = 10;
+    this.special6 = 3.5;
+    this.special7 = 7;
+  }
+  //Clear Values
+  clearAll(){
+    this.distance1 = 0;
+    this.distance2 = 0;
+    this.distance3 = 0;
+    this.distance4 = 0;
+    this.distance5 = 0;
+    this.distance6 = 0;
+    this.Income1 = 0;
+    this.Income2 = 0;
+    this.Income3 = 0; 
+    this.Income4 = 0;
+    this.Income5 = 0;
+    this.Income6 = 0;
+    this.special1 = 0;
+    this.special2 = 0;
+    this.special3 = 0;
+    this.special4 = 0;
+    this.special5 = 0;
+    this.special6 = 0;
+    this.special7 = 0;
   }
   sasa() {
     this.listOfStudents = new Array(this.students.length);
