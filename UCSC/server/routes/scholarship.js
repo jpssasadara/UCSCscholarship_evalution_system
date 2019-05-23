85139ad756545d4c973ff24918f33d178208326f
jps sasadara <jpssasadara1995@gmail.cpm>
2019-05-23 23:42:40 +0530

    changed

--- UCSC/server/routes/scholarship.js
+++ UCSC/server/routes/scholarship.js
@@ -5,5 +5,5 @@ const EnrollmentController = require('../controllers/scholarship');
 router.post('/',EnrollmentController.create_enrollment);
 router.get('/getWelfareStu',EnrollmentController.view_enrollment);
 router.get('/getWelfareStu/:stuId',EnrollmentController.view_application);
-
+router.patch('/getWelfareStu/:stuId',EnrollmentController.update_enrollment);
 module.exports=router;
